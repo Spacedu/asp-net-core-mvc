552ea08a5394fe2c778ced6ee6117a5876271670
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-08-01 20:48:02 -0300

    Update Program.cs

--- src/S04/S04/FirstProject/Program.cs
+++ src/S04/S04/FirstProject/Program.cs
@@ -1,6 +1,18 @@
+/*
+ * Program.cs + Startup.cs
+ * ASP.NET Core <5.0:
+ * - Startup.cs:
+ * -- ConfigureService = Add Services(Database, Queue, DI.....)
+ * -- Configure = Pipeline
+ * ASP.NET Core 5.0:
+ * - builder.Services = ConfigureService
+ * - app = Configure
+ */
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//Minimal API
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
