b43b07549b22575d0fd9c41e8dbcb42468de2233
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-08-01 21:04:16 -0300

    S04E04 - ASP.NET Core - Primeiros Passos

--- src/S04/S04/FirstProject/Program.cs
+++ src/S04/S04/FirstProject/Program.cs
@@ -12,7 +12,17 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//Minimal API
-app.MapGet("/", () => "Hello World!");
+app.Run(async(HttpContext context) => {
+    //Request: Address, Method, Headers, Body
+    //Response: Status Code, Header, Body
+    if(context.Request.Path.ToString()== "/contact")
+    {
+        await context.Response.WriteAsync("Contact Page!");
+    }
+    else {
+        context.Response.StatusCode = 201;
+        await context.Response.WriteAsync("Hello World! 2025");
+    }
+});
 
 app.Run();
