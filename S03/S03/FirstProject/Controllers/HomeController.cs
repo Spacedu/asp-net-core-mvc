dcb5c9d9553c7323421ff726213c889d7bbed063
Spacedu <contato@spacedu.com.br>
2025-08-12 17:59:08 -0300

    S04E02 - Primeiros passos no MVC

---
+++ S03/S03/FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers;
+
+public class HomeController : Controller
+{
+    //Razor = Engine Templates
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
