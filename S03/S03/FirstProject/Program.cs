69b892b4a696f01e80f1a48db7f65a83312ed347
Spacedu <contato@spacedu.com.br>
2025-08-12 16:14:58 -0300

    Update Program.cs

--- S03/S03/FirstProject/Program.cs
+++ S03/S03/FirstProject/Program.cs
@@ -1,7 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+/*
+ * MVC, Blazor Server, RazorPages, Database, Architecture (DI), Smtp...
+ * builder.Services.Add
+ */
 
-//Minimal API
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+/*
+ * Pipeline
+ */
+
+/*
+ * Middleware
+ * - context: Request + Response
+ * - next: > Next Middleware
+ */
+
+//Middleware 1
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Start - Middleware 1!");
+    await next.Invoke();
+    await context.Response.WriteAsync("End - Middleware 1!");
+});
+
+//Middleware 2
+app.Use(async (context, next) => {
+    await context.Response.WriteAsync("Start - Middleware 2!");
+    await next.Invoke();
+    await context.Response.WriteAsync("End - Middleware 2!");
+});
 
 app.Run();
