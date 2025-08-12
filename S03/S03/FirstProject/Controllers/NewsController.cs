f624687583cea9a5ea76516b81ae4e64af454c29
Spacedu <contato@spacedu.com.br>
2025-08-12 18:35:05 -0300

    S04E03

---
+++ S03/S03/FirstProject/Controllers/NewsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers;
+
+public class NewsController : Controller
+{
+    /*
+     * CRUD
+     */
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Add()
+    {
+        return View();
+    }
+    public IActionResult Edit()
+    {
+        return View();
+    }
+    public IActionResult Delete()
+    {
+        return Ok("Removed");
+    }
+
+    /* Client View News */
+    public IActionResult Visualize()
+    {
+        return View();
+    }
+}
