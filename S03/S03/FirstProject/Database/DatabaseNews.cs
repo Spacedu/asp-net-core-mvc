41db9abc10ed3495033acc1d9efc64ab7c55bb3d
Spacedu <contato@spacedu.com.br>
2025-08-13 16:18:52 -0300

    S04E05 - Criar Modelo

---
+++ S03/S03/FirstProject/Database/DatabaseNews.cs
@@ -0,0 +1,17 @@
+﻿using FirstProject.Models;
+
+namespace FirstProject.Database
+{
+    public class DatabaseNews
+    {
+        public static List<News> News = new List<News>()
+        {
+            new News(){ Id = 1, Title = "Title 1", Text = "Text 1" },
+            new News(){ Id = 2, Title = "Title 2", Text = "Text 2" },
+            new News(){ Id = 3, Title = "Title 3", Text = "Text 3" },
+            new News(){ Id = 4, Title = "Title 4", Text = "Text 4" },
+            new News(){ Id = 5, Title = "Title 5", Text = "Text 5" },
+            new News(){ Id = 6, Title = "Title 6", Text = "Text 6" },
+        };
+    }
+}
---
+++ S03/S03/FirstProject/Models/News.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstProject.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+    }
+}
