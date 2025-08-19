2ef31dde3db0d7fe9da0a7b755aa5b7d655af1d1
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-08-19 14:00:12 -0300

    S03E08 - Estrutura MVC - Parte 2

--- Source/Projects/LearnASPNET/Program.cs
+++ Source/Projects/LearnASPNET/Program.cs
@@ -1,19 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-//ASP.NET Core (+MVC)
-app.Run(async context => {
-    if(context.Request.Method == "GET" && context.Request.Path == "/contact")
-    {
-        context.Response.StatusCode = 200;
-        context.Response.ContentType = "text/html";
-        await context.Response.WriteAsync("<h1>Contact</h1> - Method: GET");
-    }
-    else
-    {
-        context.Response.StatusCode = 404;
-        await context.Response.WriteAsync("Not Found");
-    }
-});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "/{controller=Home}/{action=Index}/{id?}"
+);
+
+
 
 app.Run();
