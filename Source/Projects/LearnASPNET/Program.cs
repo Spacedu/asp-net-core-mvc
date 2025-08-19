5635118cfe76b2dc2cacd11d4d667bd63405430c
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-08-19 12:25:12 -0300

    S03E06 - ASPNET Core do Zero

--- Source/Projects/LearnASPNET/Program.cs
+++ Source/Projects/LearnASPNET/Program.cs
@@ -1,7 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//Minimal API (REST)
-app.MapGet("/", () => "Hello World!");
+//ASP.NET Core (+MVC)
+app.Run(async context => {
+    if(context.Request.Method == "GET" && context.Request.Path == "/contact")
+    {
+        context.Response.StatusCode = 200;
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync("<h1>Contact</h1> - Method: GET");
+    }
+    else
+    {
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsync("Not Found");
+    }
+});
 
 app.Run();
