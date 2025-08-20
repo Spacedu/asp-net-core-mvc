134064440f3940030b387b3e57c0185c2b87f4b5
ELIAS RIBEIRO DA SILVA COSTA <contato@spacedu.com.br>
2025-08-20 11:56:11 -0300

    S03E09 - Roteamento

---
+++ Source/Projects/LearnASPNET/Controllers/NewsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnASPNET.Controllers
+{
+    public class NewsController : Controller
+    {
+        //{year}/{month}/{day}/{contry}/{category}/{slug}
+        public IActionResult Visualize(int year, int month, int day, string country, string category, string slug)
+        {
+            return Ok($"year:{year}, month: {month}, day: {day}, country: {country}, category:{category}, slug:{slug}");
+        }
+    }
+}
--- Source/Projects/LearnASPNET/Program.cs
+++ Source/Projects/LearnASPNET/Program.cs
@@ -2,9 +2,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+#region News Route
+/*
+ CRUD:
+ - https://yoursite.com/Product/ (List)
+ - https://yoursite.com/Product/Add
+ - https://yoursite.com/Product/Edit/1
+
+ - https://www.nytimes.com/2025/08/19/us/politics/trump-summits-putin-zelensky.html
+ */
+app.MapControllerRoute(
+    name: "news",
+    pattern: "{year:int}/{month:int}/{day:int}/{country}/{category}/{*slug}",
+    defaults: new { controller = "News", action="Visualize"}
+);
+
+
+#endregion
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "/{controller=Home}/{action=Index}/{id?}"
+    pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 
 
